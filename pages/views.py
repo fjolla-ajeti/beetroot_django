3b1cd2fc8a998dae3a5ca7ac7eea9011c71ef92c
fjolla-ajeti <fjollaajeti9@gmail.com>
2023-12-23 21:29:27 +0100

    added-templates

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse('Hello world from django')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
